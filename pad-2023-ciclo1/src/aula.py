20414b6aadde80f27b7bf3c837fac1e7ef730e84
Daniel de Lima <dlsj.devops@gmail.com>
2023-02-21 13:51:09 -0300

    algumas linhas de codigo

---
+++ pad-2023-ciclo1/src/aula.py
@@ -0,0 +1,11 @@
+#ambientacao com vcsode
+print("ola mundo")
+
+nome = input("digite seu nome: ")
+
+print(nome)
+
+idade = input("sua idade: ")
+idade_apos = input("idade para aposentar: ")
+anos_apos = int(idade_apos) - int(idade)
+print("Faltam ", anos_apos, " para sua aposentadoria")
